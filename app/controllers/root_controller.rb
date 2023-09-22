ad1615ff677c86f5c0a4064e61058118eb63c061
unknown <teddytiyi@gmail.com>
2023-09-22 15:18:20 +0200

    fix linter

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,6 @@
 class RootController < ApplicationController
-    def index
-      random_greeting = Greeting.order('RANDOM()').first
-      render json: { greeting: random_greeting&.name }
-    end
+  def index
+    random_greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: random_greeting&.name }
   end
-  
+end
